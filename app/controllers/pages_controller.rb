97694b966a153ec717daa6c1c20d27c43901d7ef
Kollo David <kdavid77@gmail.com>
2013-09-09 17:05:26 +0300

    user creation done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,12 @@ class PagesController < ApplicationController
   end
 
   def login
+    user=User.find_by_name('dodi') 
+    if (user.nil?)
+      user=User.new(name: 'dodi',password_digest: '1a6555336f456f57ce6aa65ac2948c83')
+      user.save
+    else
+    end
+
   end
 end
