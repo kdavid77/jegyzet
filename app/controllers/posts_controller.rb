ff356212ee013acde06a66780af51069a4530a8c
Kollo David <kdavid77@gmail.com>
2013-09-10 14:50:23 +0300

    post show & edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def new
+    @post=Post.new
   end
   
   def create
@@ -11,6 +12,24 @@ class PostsController < ApplicationController
       flash[:error] = "Post failed"
     end
     redirect_to root_path
+  end
+  
+  def show
+    @post=Post.find_by_id(params[:id])
+  end
+  
+  def index
+    @allposts = Post.all
+  end
+  
+  def edit
+    @post=Post.find_by_id(params[:id])
+  end
+  
+  def update
+    @post=Post.find_by_id(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to(@post)
   end
   
   private
