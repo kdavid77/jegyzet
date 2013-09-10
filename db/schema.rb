459bba4f126a92be47e78e59232068c533f83b0c
Kollo David <kdavid77@gmail.com>
2013-09-10 20:28:00 +0300

    modify posts content from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910061955) do
+ActiveRecord::Schema.define(version: 20130910172310) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
